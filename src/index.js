1d5f69921597c6fd6e574905f7688112cc97b62d
vplacido <valentinplacido25@gmail.com>
2020-03-13 15:45:46 -0400

    created show details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLodaed", () => {
     
     fetchDogs;
+    // let pupSpan = document.querySelectorAll(".pups");
+    // pupSpan.addEventListener("click", showDetails);
+    let isGood = true;
 });
 
 function fetchDogs() {
@@ -12,6 +15,20 @@ function fetchDogs() {
 function processDog(pup) {
     let dogBar = document.querySelector("#dog-bar");
     let dog = document.createElement('span');
+    dog.className = "pups"
     dog.innerText = pup.name
     dogBar.append(dog)
+    dog.addEventListener("click", () => showDetails(pup));
+}
+
+function showDetails(event) {
+    let details = document.querySelector("#dog-info");
+    let image = document.createElement("img");
+    let name = document.createElement("h2");
+    let btn = document.createElement("button");
+    debugger;
+    image.src = event.image;
+    name = event.name 
+    
+
 }
